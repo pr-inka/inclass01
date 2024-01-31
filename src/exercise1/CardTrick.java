951c3c46b682288c9084e26f29e301173776e6ff
Prinka Prinka <prinka@sheridancollege.ca>
2024-01-31 15:26:29 -0500

    Added a parttimestudent class

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,67 +1,84 @@
 package exercise1;
 
-/**
- * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
- * It then searches the array of cards for the match to the user's card. 
- * To be used as starting code in Exercise
- *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+ * @author : Prinka
+ * @date 31 Jan, 2024
  */
 public class CardTrick {
-    
+
     public static void main(String[] args) {
-        
         Card[] hand = new Card[7];
 
+        Random random = new Random();
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
+            card.setValue(random.nextInt(13) + 1);
+            card.setSuit(random.nextInt(4));
+            hand[i] = card;
         }
 
-        // insert code to ask the user for Card value and suit, create their card
-        // and search the hand here. 
-        // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
-        // 
-        // Then loop through the cards in the array to see if there's a match.
-        
-        // If the guess is successful, invoke the printInfo() method below.
-        
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Pick a card (Enter desirable value between 1-13 and select suit between 1-4):");
+            int userValue = scanner.nextInt();
+            int userSuit = scanner.nextInt();
+            
+            boolean found = false;
+            for (Card card : hand) {
+                if (card.getValue() == userValue && card.getSuit() == userSuit - 1) {
+                    found = true;
+                    break;
+                }
+            }
+            
+            if (found) {
+                printInfo();
+            } else {
+                System.out.println("Sorry, your card is not in the hand.");
+            }
+        }
     }
 
-    /**
-     * A simple method to print out personal information. Follow the instructions to 
-     * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
-     */
     private static void printInfo() {
-    
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
-        
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Paul, but you can call me prof, Paul, or sir.");
         System.out.println();
-        
         System.out.println("My career ambitions:");
         System.out.println("-- Be more active on LinkedIn");
         System.out.println("-- Have a semester with no violations of academic integrity!");
-	System.out.println();	
-
+        System.out.println();
         System.out.println("My hobbies:");
         System.out.println("-- Investing");
         System.out.println("-- Cooking");
         System.out.println("-- Reading/Watching TV");
         System.out.println("-- Riding my motorcycle");
-
         System.out.println();
-        
-    
     }
 
+    private static class Card {
+        private int value;
+        private int suit;
+
+        public Card() {
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public int getSuit() {
+            return suit;
+        }
+
+        public void setSuit(int suit) {
+            this.suit = suit;
+        }
+    }
 }
